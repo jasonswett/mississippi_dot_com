ba0fdcbbd3876a91edc9d29d22ceb1d9fbc025d3
Jason Swett <jason@benfranklinlabs.com>
2018-10-23 11:16:25 -0400

    Install RSpec.

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,18 @@ module MississippiDotCom
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.generators do |g|
+      g.javascript_engine :js
+      g.jbuilder false
+
+      g.test_framework :rspec,
+        fixtures:         false,
+        view_specs:       false,
+        helper_specs:     false,
+        routing_specs:    false,
+        request_specs:    false,
+        controller_specs: false
+    end
   end
 end
