bfd4eca04f14550d163124438d38c73e2d532669
Jason Swett <jason@benfranklinlabs.com>
2018-10-27 10:55:56 -0400

    Add uniqueness and presence validators for Book model.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,3 @@
 class Book < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { build(:book) }
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_uniqueness_of(:name) }
+  end
 end
