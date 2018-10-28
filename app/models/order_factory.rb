27c8c59a2aaf50c3efe9c7b03ff0d28c7d921134
Jason Swett <jason@benfranklinlabs.com>
2018-10-28 14:43:36 -0400

    Refactoring.

--- app/models/order_factory.rb
+++ app/models/order_factory.rb
@@ -1,21 +1,17 @@
 class OrderFactory
   def self.build(customer_email:, book_ids: [])
-    customer = Customer.find_or_create_by(email: customer_email)
-    order = customer.orders.new
-    add_line_items(order, book_ids)
-    order
+    Order.new(
+      customer: Customer.find_or_create_by(email: customer_email),
+      line_items: line_items(book_ids.reject(&:blank?))
+    )
   end
 
-  def self.add_line_items(order, book_ids)
-    books(book_ids).each do |book|
-      order.line_items.new(
+  def self.line_items(book_ids)
+    Book.find(book_ids).map do |book|
+      LineItem.new(
         book: book,
         total_amount_cents: 0
       )
     end
-  end
-
-  def self.books(book_ids)
-    Book.find(book_ids.reject(&:blank?))
   end
 end
