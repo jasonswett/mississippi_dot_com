ebdf0fdebfd192f0c344b8f30645dfd0f6f503f2
Jason Swett <jason@benfranklinlabs.com>
2018-10-28 15:46:39 -0400

    Add feature spec for order creation with books.

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,8 @@ class Order < ApplicationRecord
   has_many :line_items
   validates :customer, presence: true
   validates :customer_id, presence: true
+
+  def summary
+    line_items.map(&:book).map(&:name).join(', ')
+  end
 end
--- spec/features/create_order_spec.rb
+++ spec/features/create_order_spec.rb
@@ -1,6 +1,21 @@
 require 'rails_helper'
 
 RSpec.feature 'Create order', type: :feature do
+  scenario 'when books are selected' do
+    create(:book, name: 'Growing Object-Oriented Software, Guided by Tests')
+    create(:book, name: 'Test-Driven Development by Example')
+
+    visit new_order_path
+    fill_in 'Customer email', with: 'test@example.com'
+    select 'Growing Object-Oriented Software, Guided by Tests', from: 'Book(s)'
+    select 'Test-Driven Development by Example', from: 'Book(s)'
+    click_on 'Create Order'
+
+    visit orders_path
+    expect(page).to have_content('Growing Object-Oriented Software, Guided by Tests')
+    expect(page).to have_content('Test-Driven Development by Example')
+  end
+
   scenario 'customer email is missing' do
     visit new_order_path
     click_on 'Create Order'
