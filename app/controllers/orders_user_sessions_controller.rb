da6f740df560ab42428da36dbfb918c8975764df
Jason Swett <jason@benfranklinlabs.com>
2019-03-12 15:40:32 -0400

    Refactor OrdersUserSessionsController some more.

--- app/controllers/orders_user_sessions_controller.rb
+++ app/controllers/orders_user_sessions_controller.rb
@@ -9,15 +9,9 @@ class OrdersUserSessionsController < ApplicationController
     end
 
     if User.exists?(email: params[:customer_email])
-      redirect_to new_orders_user_session_path(
-        customer_email: params[:customer_email],
-        book_ids: @book_ids
-      )
+      allow_user_to_sign_in
     else
-      OrderFactory.create(
-        customer_email: params[:customer_email],
-        book_ids: params[:order][:book_ids]
-      )
+      save_order
     end
   end
 
@@ -28,15 +22,30 @@ class OrdersUserSessionsController < ApplicationController
   end
 
   def create
-    user = User.find_for_authentication(email: params[:customer_email])
-    user = user&.valid_password?(params[:password]) ? user : nil
-    sign_in(user, scope: :user)
+    sign_in_user
+    save_order
+    redirect_to orders_path
+  end
 
+  private
+
+  def allow_user_to_sign_in
+    redirect_to new_orders_user_session_path(
+      customer_email: params[:customer_email],
+      book_ids: @book_ids
+    )
+  end
+
+  def save_order
     OrderFactory.create(
       customer_email: params[:customer_email],
       book_ids: params[:order][:book_ids]
     )
+  end
 
-    redirect_to orders_path
+  def sign_in_user
+    user = User.find_for_authentication(email: params[:customer_email])
+    user = user&.valid_password?(params[:password]) ? user : nil
+    sign_in(user, scope: :user)
   end
 end
