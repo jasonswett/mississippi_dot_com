859cd33f7022722480b99624b6172927bbeb5e7c
Jason Swett <jason@benfranklinlabs.com>
2019-08-11 18:08:27 -0400

    Bring test count down to 25.

--- spec/features6/view_books_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.feature 'View books', type: :feature do
-  scenario 'viewing' do
-    create(:book)
-    visit admin_books_path
-  end
-end
--- spec/features7/view_authors_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.feature 'View authors', type: :feature do
-  scenario 'viewing' do
-    create(:author)
-    visit admin_authors_path
-  end
-end
