ad374cde2a56e7aa19f2394da1b80b5aa3b67f55
Jason Swett <jason@benfranklinlabs.com>
2019-09-04 09:56:35 -0400

    Make test fail a different way.

--- spec/features/view_authors_spec.rb
+++ spec/features/view_authors_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.feature 'View authors', type: :feature do
   scenario 'viewing' do
     create(:author)
-    raise 'error'
     visit admin_authors_path
+    expect(1).to eq(2)
   end
 end
